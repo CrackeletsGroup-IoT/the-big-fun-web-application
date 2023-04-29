9ff6f25f641c35967d20e97d88c536f8f004b737
Ergotox <Diegojsf17@gmail.com>
2023-04-29 17:04:34 -0500

    feat: Faq component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
+import { FaqContentComponent } from './event/components/faq-content/faq-content.component';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import {MatCardModule} from "@angular/material/card";
     AppComponent,
     FooterContentComponent,
     NavComponent,
-    MainContentComponent
+    MainContentComponent,
+    FaqContentComponent
   ],
 
   imports: [
