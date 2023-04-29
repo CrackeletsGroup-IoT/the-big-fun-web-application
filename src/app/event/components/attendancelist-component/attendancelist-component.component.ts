2fcb4f0587f7db7213eed79edc5c698b5bf8e914
Ergotox <Diegojsf17@gmail.com>
2023-04-29 18:12:44 -0500

    feat: Attendance list component implemented

--- src/app/event/components/attendancelist-component/attendancelist-component.component.ts
+++ src/app/event/components/attendancelist-component/attendancelist-component.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./attendancelist-component.component.css']
 })
 export class AttendancelistComponentComponent {
+  asisNames =['Remi Adam Purrel','Riwsey Otiniano Lizama','Elsa Martinez Paz','Katherine Valdez Parra '];
+  asisImg=['https://randomuser.me/api/portraits/men/25.jpg','https://randomuser.me/api/portraits/men/26.jpg','https://randomuser.me/api/portraits/women/43.jpg','https://randomuser.me/api/portraits/women/78.jpg'];
 
 }
