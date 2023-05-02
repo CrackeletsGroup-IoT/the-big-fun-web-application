0a2c0d2c356f482cf263e6f23d6f72c3e2a93612
S1NGHAM <u201410580@upc.edu.pe>
2023-05-01 21:26:31 -0500

    feature: update profile component created

---
+++ src/app/event/components/update-profile-content/update-profile-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-profile-content',
+  templateUrl: './update-profile-content.component.html',
+  styleUrls: ['./update-profile-content.component.css']
+})
+export class UpdateProfileContentComponent {
+
+}
