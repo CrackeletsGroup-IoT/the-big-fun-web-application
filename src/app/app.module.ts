aec84579d5a161a2032648b915fa768bb9c10e7b
S1NGHAM <u201410580@upc.edu.pe>
2023-05-01 21:30:22 -0500

    feature: user profile component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {MatSelectModule} from "@angular/material/select";
 import {MatGridListModule} from "@angular/material/grid-list";
+import { UserProfileContentComponent } from './event/components/user-profile-content/user-profile-content.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import {MatGridListModule} from "@angular/material/grid-list";
     AppComponent,
     FooterContentComponent,
     NavComponent,
-    MainContentComponent
+    MainContentComponent,
+    UserProfileContentComponent
   ],
 
   imports: [
---
+++ src/app/event/components/user-profile-content/user-profile-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-content',
+  templateUrl: './user-profile-content.component.html',
+  styleUrls: ['./user-profile-content.component.css']
+})
+export class UserProfileContentComponent {
+
+}
