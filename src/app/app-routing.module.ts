f1600a23be7b8ed2cd3e07e71018e6efeddeae62
RauVR <u201714049@upc.edu.pe>
2023-05-03 13:06:47 -0500

    feat: App Routing Module updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes:Routes=[
   { path: 'signUp', component: UserregisterContentComponent },
   { path: 'eventRegister', component: RegistereventComponentComponent },
   { path: 'faqs', component: FaqContentComponent },
-  { path: 'eventDetail/', component: EventdetailsComponentComponent },
+  { path: 'eventDetail', component: EventdetailsComponentComponent },
 ];
 
 @NgModule({
