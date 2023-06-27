907bf9b9824822329a32a8cc6dae2185688f5b1a
Ergotox <Diegojsf17@gmail.com>
2023-06-27 02:49:18 -0500

    feat: integration updated

--- src/app/event/components/nav/nav.component.ts
+++ src/app/event/components/nav/nav.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
   isLoggedIn: boolean = false;
-  userType: string = '';
-  role=localStorage.getItem('role')
+  userType ='';
+  constructor( private router: Router) { }
+  logout() {
+
+    this.isLoggedIn = false;
+    this.userType = ''
+    this.router.navigate(['/signIn']);
+    localStorage.removeItem('role');
+  }
+  ngOnInit() {
+    interval(1000).subscribe(() => {
+      this.Login();
+
+    });
+  }
   Login(){
     this.isLoggedIn = true;
-    this.userType =  this.role.toString();
+    this.userType = localStorage.getItem('role') as string;
   }
 }
