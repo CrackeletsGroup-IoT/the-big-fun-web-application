8cfee363f3e8cf65ed18814f4f077522fb9f9618
Isabella <sorianoisabella28@gmail.com>
2024-05-26 19:30:23 -0500

    feat: implemented paymentconfirmation-content, only interface

--- src/app/event/components/paymentconfirmation-content/paymentconfirmation-content.component.ts
+++ src/app/event/components/paymentconfirmation-content/paymentconfirmation-content.component.ts
@@ -1,16 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {EventsService} from "../../services/events.service";
 
 @Component({
   selector: 'app-paymentconfirmation-content',
   templateUrl: './paymentconfirmation-content.component.html',
   styleUrls: ['./paymentconfirmation-content.component.css']
 })
-export class PaymentconfirmationContentComponent {
-  eventImg= "https://www.anayainfantilyjuvenil.com/images/libros/grande/9788469833728-la-vida-es-sueno-clasicos-hispanicos.jpg"
-  eventQr= "https://www.qr-generator.nu/qrcode.svg"
-  dataSource=[
-    {date: "25/04/2023",hour:"8:00pm", nameEvent:"Obra teatral 'La vida es un sueño'", qTikects: "2", qrCode:"https://www.qr-generator.nu/qrcode.svg"}
-  ]
-  displayedColumns: string[] = ['Fecha', 'Hora', 'Nombre', 'Tickets', 'QR'];
+export class PaymentconfirmationContentComponent implements OnInit{
 
+  eventId:any;
+  attendeeId:any;
+  events:any;
+  currentEvent:any;
+
+  eventImg:any;
+  dataSource: any;
+  displayedColumns: any;
+
+  constructor(private route:ActivatedRoute,private eventService:EventsService) {
+  }
+
+  getAllEvents() {
+    this.eventService.getAll().subscribe((response: any) => {
+      if (Array.isArray(response.content)) {
+        this.events = response.content;
+        this.findById();
+        console.log(this.events);
+      } else {
+        console.error('Invalid response format: events array not found');
+      }
+    });
+  }
+
+  findById(){
+    this.currentEvent = this.events.find((event:any)=>event.id==this.eventId);
+    console.log("evento: " + this.currentEvent);
+
+    this.eventImg= this.currentEvent.imageUrl;   //trae laimagen del back
+    this.dataSource=[
+      {date: this.currentEvent.date,hour:this.currentEvent.hour, nameEvent:this.currentEvent.name}
+    ]
+
+  }
+
+  ngOnInit(): void {
+
+    this.route.queryParams.subscribe(params=>{
+      this.eventId=params['eventId'];
+      this.attendeeId=params['attendeeId'];
+    });
+
+    this.getAllEvents();
+
+    this.displayedColumns = ['Fecha', 'Nombre'];
+  }
 }
+
